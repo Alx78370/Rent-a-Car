4e29f523d7114bbe845947dc2bd194fcf3ae215c
Alx78370 <alexis.vachet@outlook.fr>
2024-04-29 21:42:01 +0200

    modif class vehicle

--- Model/Repository/EntityRepository.php
+++ Model/Repository/EntityRepository.php
@@ -1,30 +1,33 @@
-<?php 
+<?php
 
-class EntityRepository {
-    private $pdo;
+class EntityRepository
+{
+    protected $pdo;
 
-    public function __construct($pdo) {
+    public function __construct($pdo)
+    {
 
         if ($pdo) {
             $this->pdo = $pdo;
         } else {
-            $this->pdo = new PDO ("mysql:host=localhost;dbname=rent_a_car", "root", "");
+            $this->pdo = new PDO("mysql:host=localhost;dbname=rent_a_car", "root", "");
         }
     }
 
-    public function getAll($table) {
+    public function getAll($table)
+    {
         $statement = $this->pdo->prepare("SELECT * FROM:table");
         $statement->bindParam(":table", $table);
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_CLASS, $table::class);
     }
 
-    public function getById($table, $id) {
+    public function getById($table, $id)
+    {
         $statement = $this->pdo->prepare("SELECT * FROM:table WHERE id= :id");
         $statement->bindParam(":table", $table);
         $statement->bindParam(":id", $id);
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_CLASS, $table::class);
     }
-    
 }
