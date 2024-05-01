ac659fda3c73147105d779f522c96afeb4a30e08
Maxime Yehia <yehia.maxime@hotmail.fr>
2024-05-01 10:32:27 +0200

    create header

--- Template/layout.html.php
+++ Template/layout.html.php
@@ -1,17 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Document</title>
 </head>
-<header>
-    
-</header>
-<body>
- <!-- Permet d'afficher le header, le contenue et le footer -->
-</body>
-<footer>
 
-</footer>
+<body>
+    <header>
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <a href="/">
+                    <img src="path_to_your_logo.png" class="ms-3 mt-2" alt="Logo" height="50">
+                </a>
+            </div>
+            <div class="col-md-8 d-flex justify-content-end">
+                <a href="/manage-reservations" class="nav-link me-2">Gérer mes réservations</a>
+                <a href="/login" class="nav-link me-2">Connexion</a>
+                <a href="/register" class="nav-link me-2">Inscription</a>
+            </div>
+        </div>
+    </header>
+
+    <!-- Permet d'afficher le header, le contenue et le footer -->
+
+    <footer>
+
+    </footer>
+</body>
+
 </html>
